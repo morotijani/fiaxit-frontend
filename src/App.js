67dd1c7380a9f00386ba0cdc9fe5a3ab0e429447
Tijani Moro <tjhackx111@gmail.com>
2025-03-30 23:35:37 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes as Switch, Route, Link } from 'react-router-dom';
+import AppIndex from './AppIndex';
+import AuthIndex from './components/auth/AuthIndex'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  	return (
+    	<Router>
+			<Switch>
+				<Route path="/auth" element={<AuthIndex />} />
+				<Route path="/" element={<AppIndex />} />
+			</Switch>
+    	</Router>
+  	);
 }
 
 export default App;
---
+++ src/AppIndex.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AppIndex() {
+    return(
+        <div>
+            App Index
+        </div>
+    )
+}
+
+export default AppIndex
