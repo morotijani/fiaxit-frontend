591c8e54d4f38eff96d91146397174b7f68dce92
Tijani Moro <tjhackx111@gmail.com>
2025-04-04 03:57:38 +0000

    initial commit

--- src/AppIndex.js
+++ src/AppIndex.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import MainNav from './components/MainNav'
+import Button from './components/elements/Button'
 
 function AppIndex() {
     return(
         <main>
             <MainNav />
+            <div className="main-content">
+                <Button />
+            </div>
         </main>
     )
 }
