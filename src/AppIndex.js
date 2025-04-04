2d8e41a933380ea60ba37a24269f157ce39a1c47
Tijani Moro <tjhackx111@gmail.com>
2025-04-04 11:54:12 +0000

    initial commit

--- src/AppIndex.js
+++ src/AppIndex.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MainNav from './components/MainNav'
 import Button from './components/elements/Button'
+import FieldBlock from './components/FieldBlock'
 
 function AppIndex() {
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
     return(
         <main>
             <MainNav />
             <div className="main-content">
+                <h2>Your username will be: {username}</h2>
+                <FieldBlock 
+                    id="username" value={username} onChange={(evt) => {setUsername(evt.target.value)}}
+                    label="Username:"
+                />
+                <FieldBlock
+                    id="password" value={password} onChange={(evt) => setPassword(evt.target.value)} 
+                    label="Password:" type="password" feedback="must be 8 characters !" isInvalid={true}
+                />
+
+
+
                 <Button variant="primary" onClick={() => window.alert("Button clicked")}>
                     Button One <span>%</span>
                 </Button>
