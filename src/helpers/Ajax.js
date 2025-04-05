42f7b4525ebf894307c3e112e0443c8ee4527a5f
Tijani Moro <tjhackx111@gmail.com>
2025-04-05 21:14:58 +0000

    initial commit

---
+++ src/helpers/Ajax.js
@@ -0,0 +1,60 @@
+export function runFetch(url, method, data, callback) {
+    const domain = process.env.REACT_APP_API;
+    const options = {
+        method: method, 
+        headers: {
+            'Accept': 'application/json', 
+            'Content-type': 'application/json'
+        }
+    }
+
+    //
+    if (method !== 'GET' && method !== 'DELETE') {
+        options['body'] = JSON.stringify[data];
+    }
+
+    // Add authorization header if user is logged in
+    const token = localStorage.getItem('userJWTToken');
+    if (token) {
+        options.headers['Authorization'] = `Bearer ${token}`;
+    }
+
+    // fetch
+    return fetch(domain + url, options).then(async resp => {
+        if (resp.ok) {
+            return resp.json();
+        } else if(resp.status === 422) {
+            return resp.json().then(errors => {
+                return {status: resp.status, success: false, errors: errors}
+            });
+        } else {
+            throw new Error(resp.statusText);
+        }
+    }).then(resp => {
+        if (typeof callback === 'function') {
+            return callback(resp);
+        } else {
+            return resp;
+        }
+    })
+}
+
+// post
+export function jsonPost(url, data, callback) {
+    return runFetch(url, 'POST', data, callback);
+}
+
+// get
+export function jsonGet(url, callback) {
+    return runFetch(url, 'GET', {}, callback);
+}
+
+// patch
+export function jsonPatch(url, data, callback) {
+    return runFetch(url, 'PATCH', data, callback)
+}
+
+// delete
+export function jsonDelete(url, callback) {
+    return runFetch(url, 'DELETE', {}, )
+}
