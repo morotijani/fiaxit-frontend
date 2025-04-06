1ef70d94273d6f268b6581257fdcc672bf57f646
Tijani Moro <tjhackx111@gmail.com>
2025-04-06 03:56:18 +0000

    initial commit

--- src/helpers/Form.js
+++ src/helpers/Form.js
@@ -1,9 +1,19 @@
 import {jspnPost, jsonGet, jsonPatch, jsonDelete} from './Ajax'
 
 export class Form {
-    url, 
-    fields, 
-    setFields, 
-    success,
-    method
+    url;
+    fields;
+    setFields;
+    success;
+    method;
+
+    constructor(url, fields, setFields, success, method = 'POST') {
+        this.url = url;
+        this.fields = fields;
+        this.setFields = setFields;
+        this.success = success;
+        this.method = method;
+    }
+
+    methodMap = {POST}
 }
