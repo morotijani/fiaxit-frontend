7eb011ba0829c5adf2b541647fc240feb3d8dae0
Tijani Moro <tjhackx111@gmail.com>
2025-04-09 20:20:05 +0000

    initial commit

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,12 @@
 import React, {createContext} from 'react';
 
 export const AuthContext = createContext();
+
+export function AuthStore(props) {
+    return (
+        // provide information down to our children
+        <AuthContext.Provider  value={{userId: 123}}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
