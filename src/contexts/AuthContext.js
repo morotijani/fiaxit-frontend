b9c7ce02e78d4d9c0be97df302674e316dd14cf5
Tijani Moro <tjhackx111@gmail.com>
2025-04-12 10:30:09 +0000

    initial commit

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,8 +1,18 @@
-import React, {createContext} from 'react';
+import React, {createContext, useReducer} from 'react';
 
 export const AuthContext = createContext();
 
+function reducer(store, action) {
+    switch(action.type) {
+        case 'login': 
+            localStorage.setItem(store.tokenName, action.payload);
+            return {...store, loggedIn: true}
+        default: return store;
+    }
+}
+
 export function AuthStore(props) {
+    const [store, dispatch] = useReducer();
     return(
         // provide information down to our children
         <AuthContext.Provider  value={{userId: 123}}>
