ec786f265c49fcd8c36e84a4f9d32c384bae3818
Tijani Moro <tjhackx111@gmail.com>
2025-04-17 21:34:42 +0000

    initial commit

--- src/components/todos/TodoForm.js
+++ src/components/todos/TodoForm.js
@@ -1,6 +1,34 @@
+import { useState, useContext } from 'react';
+import { Form } from '../../helpers/Form'
+import FieldBlock from '../elements/FieldBlock'
+import Button from '../elements/Button'
+import toast from 'react-hot-toast';
+
 function TodoForm() {
+
+    const [fields, setFields] = useState({
+        name: {value: "", isInvalid: false, msg: ""}, 
+        completed: {value: 0, isInvalid: false, msg: ""}
+    })
+    
+    // callback from our form submit from our backend
+    function success(resp) {
+        toast.success("Todo added successfully !");
+        console.log(resp);
+    }
+
+    // setup form
+    const form = new Form('todos/', fields, setFields, success);
+
     return (
-        <div>Todo form</div>
+        <div className="poster flex-grow-1">
+            <h3>Add new Todo </h3>
+            <FieldBlock id="name" label="Todo" isInvalid={fields.name.isInvalid} value={fields.name.value} onChange={form.handleInputChanges} feedback={fields.name.msg} />
+
+            <div className="l_footer_bottons">
+                <Button variant="primary" onClick={form.submitForm}>Create todo</Button>
+            </div>
+        </div>
     )
 }
 
