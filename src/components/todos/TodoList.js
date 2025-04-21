79edfe9a1c2302039616b0fc36ab639634f62fbe
Tijani Moro <tjhackx111@gmail.com>
2025-04-21 08:42:07 +0000

    initial commit

--- src/components/todos/TodoList.js
+++ src/components/todos/TodoList.js
@@ -1,4 +1,14 @@
+import React, { useContext } from 'react'
+import  { TodoContext } from '../../contexts/TodoContext'
+import { stringToBoolean } from '../../helpers/StringHelpers'
+import toast from 'react-hot-toast';
+import { jsonPatch, jsonDelete } from '../../helpers/Ajax'
+
 function TodoList() {
+
+    const [todoStore, todoDispatch] = useContext(TodoContext);
+    
+    
     return (
         <div>
             TodoList
